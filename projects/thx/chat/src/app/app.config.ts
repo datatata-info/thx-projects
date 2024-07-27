2f2666507a1349181a2d9edaf337262128acc892
Tomáš Javůrek <info@metazoa.org>
2024-07-27 12:18:42 +0200

    provide animations (material)

--- projects/thx/chat/src/app/app.config.ts
+++ projects/thx/chat/src/app/app.config.ts
@@ -3,6 +3,8 @@ import { provideRouter } from '@angular/router';
 import { provideThxSocket, SocketServerConfig } from '@thx/socket';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
 
 const SOCKET_SERVER_CONFIG: SocketServerConfig = {
   host: 'localhost',
@@ -14,6 +16,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideThxSocket(SOCKET_SERVER_CONFIG)
+    provideThxSocket(SOCKET_SERVER_CONFIG),
+    provideAnimationsAsync()
   ]
 };
