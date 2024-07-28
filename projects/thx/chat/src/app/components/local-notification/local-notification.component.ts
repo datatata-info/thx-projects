3525391899c78052ce4cdca5e89410fb0d82b00b
Tomáš Javůrek <info@metazoa.org>
2024-07-28 13:43:14 +0200

    init new components

---
+++ projects/thx/chat/src/app/components/local-notification/local-notification.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'thx-local-notification',
+  standalone: true,
+  imports: [],
+  templateUrl: './local-notification.component.html',
+  styleUrl: './local-notification.component.scss'
+})
+export class LocalNotificationComponent {
+
+}
