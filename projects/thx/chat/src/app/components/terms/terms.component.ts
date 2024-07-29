c1d13bfe271adcdd48b982aa26d6d8778535e181
Tomáš Javůrek <info@metazoa.org>
2024-07-29 15:55:45 +0200

    update terms

--- projects/thx/chat/src/app/components/terms/terms.component.ts
+++ projects/thx/chat/src/app/components/terms/terms.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '../../modules/material/material.module';
 import { ChatService } from '../../services/chat/chat.service';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { Location } from '@angular/common';
+import { ResearchInfoComponent } from '../research-info/research-info.component';
 
 @Component({
   selector: 'thx-terms',
   standalone: true,
-  imports: [ MaterialModule ],
+  imports: [ MaterialModule, ResearchInfoComponent, RouterModule ],
   templateUrl: './terms.component.html',
   styleUrl: './terms.component.scss'
 })
@@ -14,17 +16,22 @@ export class TermsComponent {
 
   constructor(
     private chatService: ChatService,
-    private router: Router
+    private router: Router,
+    private location: Location
   ){}
 
   acceptTerms(): void {
     this.chatService.setOption('termsApproved', true);
-    this.router.navigate(['/']);
+    this.router.navigate(['/chat']);
   }
 
   rejectTerms(): void {
     this.chatService.setOption('termsApproved', false);
     this.router.navigate(['/hello']);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
