6fbd2523e38178b90d1990811c6a7c4fdaa159fa
Tomáš Javůrek <info@metazoa.org>
2024-08-05 21:30:19 +0200

    TBD 🍀

---
+++ projects/thx/charts/src/lib/components/console/console.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'thx-charts-console',
+  standalone: true,
+  imports: [],
+  templateUrl: './console.component.html',
+  styleUrl: './console.component.scss'
+})
+
+export class ConsoleComponent {
+
+  // logOfConsole: any[] = [];
+  // log: any = console.log;
+  // warn: any = console.warn;
+  // error: any = console.error;
+  // info: any = console.info;
+
+  constructor() {
+    console.log('!!!!!!!!!! CONSOLE !!!!!!!!!!')
+    console.log = this.proxy(console, console.log, 'Log:');
+    console.warn = this.proxy(console, console.warn, 'Warn:');
+    console.error = this.proxy(console, console.error, 'Error:');
+  }
+
+  proxy(context: any, method: any, message: string) { 
+    return () => {
+      const apply = method.apply(context, [message].concat(Array.prototype.slice.apply(arguments)));
+      console.log('console context', context);
+      return apply;
+    }
+  }
+  
+}
--- projects/thx/charts/src/public-api.ts
+++ projects/thx/charts/src/public-api.ts
@@ -4,3 +4,4 @@
 
 export * from './lib/components/radar/radar.component';
 export * from './lib/components/radar/mock-data';
+export * from './lib/components/console/console.component'
