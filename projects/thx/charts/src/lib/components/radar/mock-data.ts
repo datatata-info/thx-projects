d575a1ebb4bcb4a639cb8eab472724d8257ed486
Tomáš Javůrek <xvjavurek@vutbr.cz>
2024-08-22 18:36:04 +0200

    change mock data for big five

--- projects/thx/charts/src/lib/components/radar/mock-data.ts
+++ projects/thx/charts/src/lib/components/radar/mock-data.ts
@@ -2,25 +2,27 @@ import { RadarData } from "./radar.component";
 
 export const MOCK_DATA: RadarData[] = [
     {
+      id: 'a',
       name: 'user1',
       color: 'pink',
       items: [
-        { name: 'openess', value: .3 },
+        { name: 'openess', value: .7 },
         { name: 'agreeableness', value: .8},
         { name: 'neuroticism', value: .2},
         { name: 'extraversion', value: .3},
-        { name: 'consciousness', value: .9}
+        { name: 'consciousness', value: .6}
       ]
     },
     {
+      id: 'b',
       name: 'user2',
-      color: 'yellow',
+      color: 'aquamarine',
       items: [
-        { name: 'openess', value: .3 },
-        { name: 'agreeableness', value: .8},
-        { name: 'neuroticism', value: .2},
-        { name: 'extraversion', value: .3},
-        { name: 'consciousness', value: .9}
+        { name: 'openess', value: .4 },
+        { name: 'agreeableness', value: .5},
+        { name: 'neuroticism', value: .4},
+        { name: 'extraversion', value: .6},
+        { name: 'consciousness', value: .2}
       ]
     }
   ]
